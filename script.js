ce1412f347b6eb28c9525d45ad49304bf8dfdcc7
Emre <95760128+emrdemir@users.noreply.github.com>
2023-03-27 16:31:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const results = document.getElementById('results');
 
-// Film arama fonksiyonu.
+
 async function searchMovies(searchTerm) {
   try {
     const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=4ea9c963`);
@@ -20,23 +20,16 @@ async function searchMovies(searchTerm) {
               <img src="${movie.Poster}" alt="${movie.Title} poster">
             </a>
           </div>
-          <div class="details">
-            <h2 class="title">${movie.Title}</h2>
-            <p class="year">${movie.Year}</p>
-            <p class="plot">${movie.Plot}</p>
-            <p class="director"><strong>Director:</strong> ${movie.Director}</p>
-            <p class="actors"><strong>Actors:</strong> ${movie.Actors}</p>
-          </div>
         </div>
       `;
     }).join('');
+    redirectToIMDB();
   } catch (error) {
     console.error(error);
     results.innerHTML = '<p>There was an error fetching the data.</p>';
   }
 }
 
-// Film arama formuna submit event listener'ı ekleme.
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const searchTerm = searchInput.value.trim();
@@ -46,7 +39,7 @@ form.addEventListener('submit', (event) => {
   searchMovies(searchTerm);
 });
 
-// Film detaylarına yönlendirme fonksiyonu.
+
 function redirectToIMDB() {
   const movieElements = document.querySelectorAll('.movie');
   movieElements.forEach(movie => {
@@ -56,6 +49,3 @@ function redirectToIMDB() {
     });
   });
 }
-
-
-redirectToIMDB();
